0873e96dc6da6a9abf813cd14b03113c18ad7752
DrInfinite <work.naparajith@gmail.com>
2024-04-02 16:26:43 +0530

    chore: moved to npm from pnpm and from vscode to nvim
    - Simplifying the workflow to use npm instead of pnpm
    - Left VSCode in favor of NeoVim
    - Using lazygit instead of Git CLI

--- prettier.config.js
+++ prettier.config.js
@@ -1,16 +1,16 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 module.exports = {
-    plugins: ['prettier-plugin-tailwindcss'],
-    trailingComma: 'es5',
-    bracketSpacing: true,
-    tabWidth: 4,
-    semi: true,
-    printWidth: 80,
-    singleQuote: true,
-    jsxSingleQuote: false,
-    useTabs: false,
-    arrowParens: 'always',
-    /*  overrides: [
+  plugins: ['prettier-plugin-tailwindcss'],
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  tabWidth: 2,
+  semi: true,
+  printWidth: 80,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  useTabs: false,
+  arrowParens: 'always',
+  /*  overrides: [
         {
             files: "Routes.*",
             options: {
