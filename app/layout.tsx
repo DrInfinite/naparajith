9612009caa137996fb6f6793e9f423ac1e6c65e7
DrInfinite <work.naparajith@gmail.com>
2024-05-15 00:41:36 +0530

    feat: updated site wide default font
    - Changed the default font across the site to JetBrains Mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,48 +1,51 @@
-import { ModeToggle } from '@/components/mode-toggle';
-import { ThemeProvider } from '@/components/theme-provider';
-import { Toaster } from '@/components/ui/sonner';
-import type { Metadata } from 'next';
-import './globals.css';
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import type { Metadata } from "next";
+import { JetBrains_Mono } from "next/font/google";
+import "./globals.css";
 
 export const metadata: Metadata = {
-    title: {
-        default: 'Portfolio',
-        template: '%s | Naparajith',
-    },
-    description:
-        'The personal portfolio of T L Naparajith, a CSE Undergrad and software engineer from India',
-    authors: [
-        {
-            name: 'DrInfinite',
-            url: 'https://github.com/DrInfinite',
-        },
-        {
-            name: 'Naparajith T L',
-            url: 'https://in.linkedin.com/in/naparajith',
-        },
-    ],
-    creator: 'Naparajith',
-    publisher: 'Naparajith',
+	title: {
+		default: "Portfolio",
+		template: "%s | Naparajith",
+	},
+	description:
+		"The personal portfolio of T L Naparajith, a CSE Undergrad and software engineer from India",
+	authors: [
+		{
+			name: "DrInfinite",
+			url: "https://github.com/DrInfinite",
+		},
+		{
+			name: "Naparajith T L",
+			url: "https://in.linkedin.com/in/naparajith",
+		},
+	],
+	creator: "Naparajith",
+	publisher: "Naparajith",
 };
 
+const jbMono = JetBrains_Mono({ subsets: ["latin"] });
+
 export default function RootLayout({
-    children,
+	children,
 }: {
-    children: React.ReactNode;
+	children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" suppressHydrationWarning>
-            <body className="">
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="system"
-                    enableSystem
-                >
-                    <Toaster position="bottom-right" richColors />
-                    {children}
-                    <ModeToggle />
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${jbMono.className}`}>
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+				>
+					<Toaster position="bottom-right" richColors />
+					{children}
+					<ModeToggle />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
 }
