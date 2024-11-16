f1b7ce9250c3b8582926004aa1aa5e33d946ea82
DrInfinite <work.naparajith@gmail.com>
2024-11-16 10:48:10 +0530

    feat: improved image optimization and new prefetching strategy

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,18 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+	adapter: vercel(),
+	image: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
+		],
+	},
 	integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+	output: "hybrid",
+	prefetch: { prefetchAll: true, defaultStrategy: "load" },
+	redirects: { "/links": "/quick-view" },
 	site:
 		process.env.NODE_ENV === "development"
 			? "http://localhost:4321/"
 			: "https://naparajith.vercel.app/",
-	output: "hybrid",
-	adapter: vercel(),
-	redirects: { "/links": "/quick-view" },
 });
