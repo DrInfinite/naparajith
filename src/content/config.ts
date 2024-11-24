687d7d74e2e52cbf2ba09d600234fdbd9cf9dcd0
DrInfinite <work.naparajith@gmail.com>
2024-11-24 23:08:48 +0530

    chore: fixed whitespace formatting

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,42 +1,42 @@
 import { defineCollection, z } from "astro:content";
 
 const policy = defineCollection({
-    schema: z.object({
-        title: z.string(),
-        description: z.string(),
-        created: z.coerce.date(),
-        updated: z.coerce.date().optional(),
-    }),
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		created: z.coerce.date(),
+		updated: z.coerce.date().optional(),
+	}),
 });
 
 const quotes = defineCollection({
-    type: "data",
-    schema: z.object({
-        cite: z.string(),
-        quote: z.string(),
-        author: z.string(),
-    }),
+	type: "data",
+	schema: z.object({
+		cite: z.string(),
+		quote: z.string(),
+		author: z.string(),
+	}),
 });
 
 const bio = defineCollection({ schema: z.object({ title: z.string() }) });
 
 const legends = defineCollection({
-    schema: z.object({
-        name: z.string(),
-        full_name: z.string(),
-        popular_name: z.string(),
-        born: z.union([z.coerce.date(), z.string()]),
-        died: z.union([z.coerce.date(), z.string()]),
-        description: z.string(),
-        opinion: z.string(),
-        image: z.string(),
-        image_source: z.string(),
-    }),
+	schema: z.object({
+		name: z.string(),
+		full_name: z.string(),
+		popular_name: z.string(),
+		born: z.union([z.coerce.date(), z.string()]),
+		died: z.union([z.coerce.date(), z.string()]),
+		description: z.string(),
+		opinion: z.string(),
+		image: z.string(),
+		image_source: z.string(),
+	}),
 });
 
 export const collections = {
-    "indian-legends": legends,
-    bio: bio,
-    policy: policy,
-    quotes: quotes,
+	"indian-legends": legends,
+	bio: bio,
+	policy: policy,
+	quotes: quotes,
 };
