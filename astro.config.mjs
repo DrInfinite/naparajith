c1c4e844cfdd6bce51dbf66102eedb71edf7b37a
DrInfinite <naparajith@duck.com>
2025-03-14 20:37:10 +0530

    chore: migrated to v2 content collection

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,46 +32,46 @@ import ReadingTime from "./reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel({ isr: true }),
-  image: {
-    remotePatterns: [
-      { protocol: "https", hostname: "avatars.githubusercontent.com" },
-      { protocol: "https", hostname: "upload.wikimedia.org" },
-      { protocol: "https", hostname: "external-content.duckduckgo.com" },
-      { protocol: "https", hostname: "brainmade.org" },
-      { protocol: "https", hostname: "assets.leetcode.com" },
-    ],
-    service: {
-      entrypoint: "astro/assets/services/sharp",
-      config: {
-        limitInputPixels: true,
-      },
-    },
-    experimentalLayout: "responsive",
-    experimentalObjectPosition: "center",
-    experimentalObjectFit: "cover",
-  },
-  integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
-  output: "static",
-  markdown: {
-    remarkPlugins: [ReadingTime],
-    syntaxHighlight: "shiki",
-    shikiConfig: { theme: "rose-pine", langAlias: { cc: "cpp" } },
-  },
-  prefetch: { prefetchAll: true, defaultStrategy: "viewport" },
-  redirects: {
-    "/links": "/quick-view",
-    "/bio": "/profile",
-    "/indian-legends": "/legends",
-    "/quotes": "/quotes-i-like",
-    "/contact": "/quick-view",
-  },
-  experimental: {
-    clientPrerender: true,
-    responsiveImages: true,
-  },
-  site:
-    process.env.NODE_ENV === "development"
-      ? "http://localhost:4321/"
-      : "https://naparajith.in/",
+	adapter: vercel({ isr: true }),
+	image: {
+		remotePatterns: [
+			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
+			{ protocol: "https", hostname: "upload.wikimedia.org" },
+			{ protocol: "https", hostname: "external-content.duckduckgo.com" },
+			{ protocol: "https", hostname: "brainmade.org" },
+			{ protocol: "https", hostname: "assets.leetcode.com" },
+		],
+		service: {
+			entrypoint: "astro/assets/services/sharp",
+			config: {
+				limitInputPixels: true,
+			},
+		},
+		experimentalLayout: "responsive",
+		experimentalObjectPosition: "center",
+		experimentalObjectFit: "cover",
+	},
+	integrations: [tailwind({ applyBaseStyles: false }), sitemap()],
+	output: "static",
+	markdown: {
+		remarkPlugins: [ReadingTime],
+		syntaxHighlight: "shiki",
+		shikiConfig: { theme: "rose-pine", langAlias: { cc: "cpp" } },
+	},
+	prefetch: { prefetchAll: true, defaultStrategy: "viewport" },
+	redirects: {
+		"/links": "/quick-view",
+		"/bio": "/profile",
+		"/indian-legends": "/legends",
+		"/quotes": "/quotes-i-like",
+		"/contact": "/quick-view",
+	},
+	experimental: {
+		clientPrerender: true,
+		responsiveImages: true,
+	},
+	site:
+		process.env.NODE_ENV === "development"
+			? "http://localhost:4321/"
+			: "https://naparajith.in/",
 });
