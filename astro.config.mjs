c1997f168d51e83c5e5e177df67afc66f46d38ce
DrInfinite <naparajith@duck.com>
2025-03-19 00:32:56 +0530

    fix: removed isr

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ import ReadingTime from "./reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	adapter: vercel({ isr: { expiration: 60 * 60 } }),
+	adapter: vercel(),
 	image: {
 		remotePatterns: [
 			{ protocol: "https", hostname: "avatars.githubusercontent.com" },
