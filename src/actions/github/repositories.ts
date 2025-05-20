0e2b3ffcba6da12e41a4296add949c6eb88e2b1c
DrInfinite <naparajith@duck.com>
2025-05-20 10:54:34 +0530

    fix: added missing actions

---
+++ src/actions/github/repositories.ts
@@ -0,0 +1,139 @@
+/**
+ * This website is only meant to showcase the work and and skills of the author,
+ * on a professional level. It also has a blog, containing the author's observations
+ * and opinions on various topics. The views expressed are the author's own.
+ * Copyright (C) 2024  T L Naparajith
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License Version 3 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/agpl-3.0.txt>.
+ *
+ * Contact me through electronic mail: <naparajith@duck.com>
+ */
+
+export type TGithubRepositoryProps = {
+	id: number;
+	node_id: string;
+	name: string;
+	full_name: string;
+	private: boolean;
+	owner: TOwnerProps;
+	html_url: string;
+	description: string;
+	fork: boolean;
+	url: string;
+	forks_url: string;
+	keys_url: string;
+	collaborators_url: string;
+	teams_url: string;
+	hooks_url: string;
+	issue_events_url: string;
+	events_url: string;
+	assignees_url: string;
+	branches_url: string;
+	tags_url: string;
+	blobs_url: string;
+	git_tags_url: string;
+	git_refs_url: string;
+	trees_url: string;
+	statuses_url: string;
+	languages_url: string;
+	stargazers_url: string;
+	contributors_url: string;
+	subscribers_url: string;
+	subscription_url: string;
+	commits_url: string;
+	git_commits_url: string;
+	comments_url: string;
+	issue_comment_url: string;
+	contents_url: string;
+	compare_url: string;
+	merges_url: string;
+	archive_url: string;
+	downloads_url: string;
+	issues_url: string;
+	pulls_url: string;
+	milestones_url: string;
+	notifications_url: string;
+	labels_url: string;
+	releases_url: string;
+	deployments_url: string;
+	created_at: Date;
+	updated_at: Date;
+	pushed_at: Date;
+	git_url: string;
+	ssh_url: string;
+	clone_url: string;
+	svn_url: string;
+	homepage: string;
+	size: number;
+	stargazers_count: number;
+	watchers_count: number;
+	language: string;
+	has_issues: boolean;
+	has_projects: boolean;
+	has_downloads: boolean;
+	has_wiki: boolean;
+	has_pages: boolean;
+	has_discussions: boolean;
+	forks_count: number;
+	mirror_url: null;
+	archived: boolean;
+	disabled: boolean;
+	open_issues_count: number;
+	license: TLicenseProps;
+	allow_forking: boolean;
+	is_template: boolean;
+	web_commit_signoff_required: boolean;
+	topics: string[];
+	visibility: string;
+	forks: number;
+	open_issues: number;
+	watchers: number;
+	default_branch: string;
+};
+
+export type TLicenseProps = {
+	key: string;
+	name: string;
+	spdx_id: string;
+	url: string;
+	node_id: string;
+};
+
+export type TOwnerProps = {
+	login: string;
+	id: number;
+	node_id: string;
+	avatar_url: string;
+	gravatar_id: string;
+	url: string;
+	html_url: string;
+	followers_url: string;
+	following_url: string;
+	gists_url: string;
+	starred_url: string;
+	subscriptions_url: string;
+	organizations_url: string;
+	repos_url: string;
+	events_url: string;
+	received_events_url: string;
+	type: string;
+	user_view_type: string;
+	site_admin: boolean;
+};
+
+export async function GithubRepositories() {
+	const response = await fetch("https://api.github.com/users/DrInfinite/repos");
+	const repositories: TGithubRepositoryProps[] = await response.json();
+
+	return repositories;
+}
